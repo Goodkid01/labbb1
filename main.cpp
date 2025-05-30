a3247f2a5f739586ecdf442b08c3696212170eb2
Goodkid01 <zaikinbkmkop@gmail.com>
2025-05-31 00:13:50 +0200

    1

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+#include <cfloat>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+using std::cout;
+
+
+
+
+int main() 
+{
+    const int c=5;
+    int mas[c];
+    int n=3;
+    for(int i=0;i<n;i++)
+    {
+        std::cin>>mas[i];
+    }
+    for (int i=0;i<n;i++)
+    {
+        
+    }
+
+    
+}
+
